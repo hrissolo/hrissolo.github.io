f053b97b867e8f23aff0d639f80b7b16489231ad
hrissolo <hrissolo@gmail.com>
2020-12-28 11:56:24 -0600

    footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css"
+
+export const Footer = props => {
+    return (
+        <>
+            <div className="footerSection">
+                Hailey Rissolo 2020 | hrissolo@gmail.com 
+            </div>
+        </>
+    );
+};
