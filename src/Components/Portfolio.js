2f54036ba88d026f684df4e46a0eeb6bcd32e006
hrissolo <hrissolo@gmail.com>
2021-01-12 15:44:57 -0600

    resize and added github link to reptiromance

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,5 +1,4 @@
 import React from "react"
-import {Route, Redirect} from "react-router-dom"
 import {ApplicationViews} from "../App"
 import { Footer } from "./Footer/Footer"
 import {NavBar} from "./NavBar/NavBar"
--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,25 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import work1 from "./work-imgs/ReptiRomance.png"
-
 import "./Work.css"
-import {Card, Icon, Image} from 'semantic-ui-react'
 
 export const Work = props => {
     return (
         <>
             <div className="workSection">
                 <h2>Recent Work</h2>
-                <div className="workCards">
-                    <Card>
-                    <Image className="workBox" src={work1} wrapped ui={false}></Image>
-                        <Card.Content>
-                            <Card.Header>Repti-Romance</Card.Header>
-                            <Card.Description>October 2020 Capstone</Card.Description>
-                            <Link to="/reptiromance">Read more!</Link>
-                        </Card.Content>
-                    </Card>
-                </div>
+                <section className="workCards">
+                    <div className="workCard1">
+                        <Link to="/reptiromance">
+                        <img className="workBox" src={work1} wrapped ui={false}></img>
+                        </Link>
+                        <div className="title">Repti-Romance</div>
+                        <p>October 2020 | Capstone <br/>
+                        <Link to="/reptiromance" >Read more!</Link></p>
+                    </div>
+                    
+                </section>
             </div>
         </>
     );
