b90028676be5dc49bd959a6e6d42a8e253d440e5
hrissolo <hrissolo@gmail.com>
2021-01-13 10:02:23 -0600

    changed to hash router to make this page live

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ApplicationViews from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 import { HaileyPortfolio } from './Components/Portfolio';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/">
       <HaileyPortfolio />
     </Router>
   </React.StrictMode>,
